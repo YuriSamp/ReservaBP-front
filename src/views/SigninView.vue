d3cf44447bb6fc8b90f89b523542a33f72c919db
YuriSamp <yurisamp123@gmail.com>
2024-01-27 13:39:08 -0300

    feat: add form control to forms

--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -1,26 +1,34 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { Form, Field } from 'vee-validate'
+
+const onSubmit = (values: any) => {
+  console.log(values)
+}
 </script>
 
 <template>
   <main class="flex h-full justify-center items-center">
     <div class="flex items-center justify-center w-80">
-      <div class="flex flex-col gap-4 w-full">
+      <Form class="flex flex-col gap-4 w-full" @submit="onSubmit">
         <h2 class="flex justify-center text-3xl text-white">Welcome back</h2>
         <div class="flex flex-col gap-2">
           <label>Email</label>
-          <input class="bg-transparent rounded-md border border-gray-600 py-1 px-3" />
+          <Field name="Email" class="bg-transparent rounded-md border border-gray-600 py-1 px-3" />
         </div>
         <div class="flex flex-col gap-2">
           <label>Password</label>
-          <input class="bg-transparent rounded-md border border-gray-600 py-1 px-3" />
+          <Field
+            name="Password"
+            class="bg-transparent rounded-md border border-gray-600 py-1 px-3"
+          />
         </div>
         <button class="w-full bg-[#379968] text-white h-10 rounded-md">Sign in</button>
         <p>
           Don't have an account?
           <RouterLink to="/signup">Sign up now</RouterLink>
         </p>
-      </div>
+      </Form>
     </div>
   </main>
 </template>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,30 +1,43 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { Form, Field } from 'vee-validate'
+
+const onSubmit = (values: any) => {
+  console.log(values)
+}
 </script>
 
 <template>
   <main class="flex h-full justify-center items-center">
     <div class="h-full flex items-center justify-center w-80">
-      <div class="flex flex-col gap-4 w-full">
+      <Form class="flex flex-col gap-4 w-full" @submit="onSubmit">
         <h2 class="flex justify-center text-3xl text-white">Create an account</h2>
         <div class="flex flex-col gap-2">
           <label>Email</label>
-          <input class="bg-transparent rounded-md border border-gray-600 py-1 px-3" />
+          <Field name="Email" class="bg-transparent rounded-md border border-gray-600 py-1 px-3" />
         </div>
         <div class="flex flex-col gap-2">
           <label>Password</label>
-          <input class="bg-transparent rounded-md border border-gray-600 py-1 px-3" />
+          <Field
+            name="Password"
+            type="password"
+            class="bg-transparent rounded-md border border-gray-600 py-1 px-3"
+          />
         </div>
         <div class="flex flex-col gap-2">
           <label>Confirm the password</label>
-          <input class="bg-transparent rounded-md border border-gray-600 py-1 px-3" />
+          <Field
+            name="ConfirmPassword"
+            type="password"
+            class="bg-transparent rounded-md border border-gray-600 py-1 px-3"
+          />
         </div>
         <button class="w-full bg-[#379968] text-white h-10 rounded-md">Sign up</button>
         <p>
           Have an account?
           <RouterLink to="/signin">Sign in now</RouterLink>
         </p>
-      </div>
+      </Form>
     </div>
   </main>
 </template>
