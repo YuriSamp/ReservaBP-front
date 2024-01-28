84d58cd15fe81f362c493271f01c58974a738467
YuriSamp <yurisamp123@gmail.com>
2024-01-28 17:47:06 -0300

    feat:add shadcn ui

--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -1,26 +1,28 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRouter } from 'vue-router'
 import axios from 'axios'
 import { ref } from 'vue'
 
 const email = ref(null)
 const password = ref(null)
-
-console.log(email.value, password.value)
+const router = useRouter()
 
 const onSubmit = async (e: Event) => {
   e.preventDefault()
-  const values = {
-    email: email.value,
-    password: password.value
-  }
-  const { data } = await axios.post('http://localhost:8000/signin', { ...values })
-  console.log(data)
+  try {
+    const values = {
+      email: email.value,
+      password: password.value
+    }
+    const { data, status } = await axios.post('http://localhost:8000/signin', { ...values })
+
+    router.push('/dashboard')
+  } catch (error) {}
 }
 </script>
 
 <template>
-  <main class="flex h-[100vh] justify-center items-center">
+  <main class="flex h-full justify-center items-center">
     <div class="flex items-center justify-center w-80">
       <form class="flex flex-col gap-4 w-full" @submit="onSubmit">
         <h2 class="flex justify-center text-3xl text-white">Welcome back</h2>
@@ -46,7 +48,7 @@ const onSubmit = async (e: Event) => {
         <button class="w-full bg-[#379968] text-white h-10 rounded-md">Sign in</button>
         <p>
           Don't have an account?
-          <RouterLink to="/signup">Sign up now</RouterLink>
+          <RouterLink to="/signup" class="text-[#00bd7e]">Sign up now</RouterLink>
         </p>
       </form>
     </div>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -14,46 +14,67 @@ const onSubmit = async (e: Event) => {
     password: password.value,
     confirmPassword: confirmPassword.value
   }
-  const { data } = await axios.post('http://localhost:8000/signin', { ...values })
-  console.log(values)
+  const { status } = await axios.post('http://localhost:8000/signin', { ...values })
+  console.log(status)
 }
 </script>
 
 <template>
-  <main class="flex h-full justify-center items-center">
+  <main class="flex h-full justify-center items-center overflow-y-hidden">
     <div class="h-full flex items-center justify-center w-80">
       <form class="flex flex-col gap-4 w-full" @submit="onSubmit">
         <h2 class="flex justify-center text-3xl text-white">Create an account</h2>
         <div class="flex flex-col gap-2">
-          <label>Email</label>
+          <label>Email *</label>
           <input
             name="email"
             v-model="email"
             class="bg-transparent rounded-md border border-gray-600 py-1 px-3"
+            placeholder="Jhon Doe"
           />
         </div>
         <div class="flex flex-col gap-2">
-          <label>Password</label>
+          <label>Nome *</label>
+          <input
+            name="email"
+            v-model="email"
+            class="bg-transparent rounded-md border border-gray-600 py-1 px-3"
+            placeholder="johndoe@gmail.com"
+          />
+        </div>
+        <div class="flex flex-col gap-2">
+          <label>Profile picture</label>
+          <input
+            name="email"
+            v-model="email"
+            class="bg-transparent rounded-md border border-gray-600 py-1 px-3"
+            placeholder="place an url"
+          />
+        </div>
+        <div class="flex flex-col gap-2">
+          <label>Password *</label>
           <input
             name="password"
             type="password"
             v-model="password"
             class="bg-transparent rounded-md border border-gray-600 py-1 px-3"
+            placeholder="my coolest password"
           />
         </div>
         <div class="flex flex-col gap-2">
-          <label>Confirm the password</label>
+          <label>Confirm the password *</label>
           <input
             name="confirmPassword"
             type="password"
             v-model="confirmPassword"
             class="bg-transparent rounded-md border border-gray-600 py-1 px-3"
+            placeholder="my coolest password"
           />
         </div>
         <button class="w-full bg-[#379968] text-white h-10 rounded-md">Sign up</button>
         <p>
           Have an account?
-          <RouterLink to="/">Sign in now</RouterLink>
+          <RouterLink to="/" class="text-[#00bd7e]">Sign in now</RouterLink>
         </p>
       </form>
     </div>
